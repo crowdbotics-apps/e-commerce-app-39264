f40ec06636b4eab11faed40ee5a94f977b78bd2a
Waqas Ahmed <waqas.ahmed@crowdbotics.com>
2023-03-07 23:21:56 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "e-commerce-app-39264",
   url: "https://e-commerce-app-39264.botics.co",
   api: "https://e-commerce-app-39264.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
